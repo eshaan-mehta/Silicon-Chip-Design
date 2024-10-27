eef0bf3716e4bfd2f87cae4f117104652025bf6b
Eshaan Mehta <eshaanm.mehta@gmail.com>
2024-10-26 21:18:52 -0400

    zyft approach

--- src/project.v
+++ src/project.v
@@ -8,9 +8,14 @@ module tt_um_dff_mem_eshaanmehta #(
     output reg [7:0] data_out,   // Data output
     input wire ce_n,             // Active-low Chip Enable signal (RAM drives the bus)
     input wire lr_n,             // Active-low Load RAM signal (RAM loads from MAR)
-    input wire clk,               // Clock signal
+    input  wire [7:0] ui_in,    // Dedicated inputs - address
+    output reg  [7:0] uo_out,   // Dedicated outputs - not used
+    input  wire [7:0] uio_in,   // IOs: write data
+    output reg [7:0] uio_out,  // IOs: read data
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output), not used
     input wire ena,      // will go high when the design is enabled
     input wire rst_n,     // reset_n - low to reset
+    input wire clk        //for clock sychroniztion
 );
     localparam addr_bits = 4;
  
