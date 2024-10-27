27fd10ba535755fd605cf6b205fd58ef8cc64de8
Eshaan Mehta <eshaanm.mehta@gmail.com>
2024-10-26 21:45:41 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,33 +1,37 @@
 `default_nettype none
- 
-module tt_um_dff_mem_eshaanmehta #(
+
+module tt_um_dff_mem #(
     parameter RAM_BYTES = 16
 ) (
-    input wire [3:0] mar,        // Adress from RAM - 4-bit for 16-byte RAM
-    input wire [7:0] data_in,    // Data input
-    output reg [7:0] data_out,   // Data output
-    input wire ce_n,             // Active-low Chip Enable signal (RAM drives the bus)
-    input wire lr_n,             // Active-low Load RAM signal (RAM loads from MAR)
     input  wire [7:0] ui_in,    // Dedicated inputs - address
     output reg  [7:0] uo_out,   // Dedicated outputs - not used
     input  wire [7:0] uio_in,   // IOs: write data
     output reg [7:0] uio_out,  // IOs: read data
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output), not used
-    input wire ena,      // will go high when the design is enabled
-    input wire rst_n,     // reset_n - low to reset
-    input wire clk        //for clock sychroniztion
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       rst_n,     // reset_n - low to reset
+    input  wire       clk        //for clock sychroniztion
 );
-    localparam addr_bits = 4;
- 
+    
+  wire [3:0] addr = ui_in[3:0];
+  wire ce_n = ui_in[7];
+  wire lr_n = ui_in[6];
+    
+  // assign uio_oe  = 8'b0;  
+  // assign uio_out = 8'b0;
+
   reg [7:0] RAM[RAM_BYTES - 1:0];
- 
+
   always @(posedge clk) begin
-      if (!lr_n) begin           // Write to RAM when lr is low (active-low)
-          RAM[mar] <= data_in;
-      end else begin           // Read from RAM when lr is high (inactive)
-          if (!ce_n) begin       // RAM can drive the bus only when ce is low
-              data_out <= RAM[mar];
+      // case 1: write to ram
+      if (!lr_n) begin
+          RAM[addr] <= uio_in; 
+      end else begin
+      // case 2: read from ram
+          if(!ce_n) begin
+              uio_out <= RAM[addr];
           end
       end
   end
-endmodule
+
+endmodule  // tt_um_dff_mem
