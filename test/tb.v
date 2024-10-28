35837e07c3c758134dd9fad5719874b9c5ab6eff
Darius Rudaitis <81165464+Troops3@users.noreply.github.com>
2024-10-28 14:01:39 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_dff_mem_eshaanmehta user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -45,3 +45,5 @@ module tb ();
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
+
+endmodule
