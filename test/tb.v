af88a0474166f50a0eef021c0c317748ed0a550a
Darius Rudaitis <81165464+Troops3@users.noreply.github.com>
2024-10-28 13:43:19 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,42 +1,47 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
 module tb ();
 
-    // This part dumps the trace to a VCD file that can be viewed with GTKWave
-    initial begin
-        $dumpfile ("tb.vcd");
-        $dumpvars (0, tb);
-        #1;  // Minimal delay to ensure dump happens
-        $finish;  // End the simulation early, as we don't need to run any tests
-    end
+  // Dump the signals to a VCD file. You can view it with gtkwave.
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    #1;
+  end
 
-    // Wire up the inputs and outputs:
-    reg clk;
-    reg rst_n;
-    reg ena;
-    reg [6:0] addr;  // Address
-    reg we;          // Write enable
-    reg [7:0] data_in;
-    wire [7:0] data_out;
-    wire [7:0] bidirectional_is_output;
+  // Wire up the inputs and outputs:
+  reg clk;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
+`ifdef GL_TEST
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
 
-    // Clock generation (though we aren't driving any signals, it's good to define it)
-    always #5 clk = ~clk;
+  // Replace tt_um_example with your module name:
+  tt_um_example user_project (
 
-    // Instantiate the DUT (Device Under Test)
-    tt_um_dff_mem_eshaanmehta user_project(
-    `ifdef GL_TEST
-        .VPWR( 1'b1),
-        .VGND( 1'b0),
-    `endif
-        .ui_in  ({we, addr}),   // Combine write enable and address
-        .uo_out (data_out),     // Data output
-        .uio_in (data_in),      // Data input (for writing)
-        .uio_oe (bidirectional_is_output),  // Not used, bidirectional enable
-        .ena    (ena),          // Enable
-        .clk    (clk),          // Clock
-        .rst_n  (rst_n)         // Reset (active low)
-    );
+      // Include power ports for the Gate Level test:
+`ifdef GL_TEST
+      .VPWR(VPWR),
+      .VGND(VGND),
+`endif
 
-endmodule
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
+  );
